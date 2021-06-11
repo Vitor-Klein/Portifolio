8d2f8669831fbdbfd9abe778f43bd49f4cf4f526
Vitor Klein <vitorhklein2@gmail.com>
2021-06-11 17:43:57 -0300

    make header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,21 @@ import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Vitor Hugo Klein</title>
       </Head>
 
-      <main className={styles.texts}>
-        <h1>Hello World</h1>
-      </main>
+      <div className={styles.content}>
+        <header>
+          <h2>VitorK</h2>
+          <div className="icons">
+            <img src="/linkedin-icon.svg" alt="linkedin" />
+            <img src="/insta-icon.svg" alt="insta" />
+            <img src="/git-icon.svg" alt="git" />
+          </div>
+        </header>
+      </div>
     </div>
 
   )
