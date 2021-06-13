7e7f1ae15017040b1e8a1d891556f03376b14730
Vitor Klein <vitorhklein2@gmail.com>
2021-06-13 17:21:42 -0300

    create first repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,15 @@ export default function Home() {
               width={400}
               style={{ marginLeft: '-21px' }}
             />
-            <h1>My repositóries</h1>
+            <h1>My Top repositóries</h1>
+          </div>
+
+          <div className={styles.repo} onClick={() => {}}>
+            <div className={styles.repoTexts}>
+              <h1>Be-The-Hero</h1>
+              <h2>MAR 28, 2020 </h2>
+            </div>
+            <hr />
           </div>
 
         </div>
