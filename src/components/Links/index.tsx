6389e2b210da62593fac99b649c511a26a3c592d
Vitor Klein <vitorhklein2@gmail.com>
2021-06-17 21:33:18 -0300

    add href to buttons

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 import styles from "./links.module.scss";
 
@@ -6,19 +7,27 @@ const Links: React.FC = () => {
   return (
     <>
       <div className={styles.link}>
-            <h1>Links to contact-me</h1>
-            <div className={styles.buttons}>
-              <button style={{ backgroundColor: "var(--red)" }}>
-                <img src="/gmail-icon.svg" alt="twitter" />
-              </button>
-              <button style={{ backgroundColor: "var(--blue)" }}>
-                <img src="/twitter-icon.svg" alt="twitter" />
-              </button>
-              <button style={{ backgroundColor: "var(--purple)" }}>
-                <img src="/rocket-icon.svg" alt="twitter" />
-              </button>
-            </div>
-          </div>
+        <h1>Links to contact-me</h1>
+        <div className={styles.buttons}>
+          <Link href="mailto:vitorhklein2@gmail.com">
+            <button style={{ backgroundColor: "var(--red)" }}>
+              <img src="/gmail-icon.svg" alt="twitter" />
+            </button>
+          </Link>
+
+          <Link href="https://twitter.com/VitorHKlein">
+            <button style={{ backgroundColor: "var(--blue)" }}>
+              <img src="/twitter-icon.svg" alt="twitter" />
+            </button>
+          </Link>
+
+          <Link href="https://app.rocketseat.com.br/me/vitor-klein">
+            <button style={{ backgroundColor: "var(--purple)" }}>
+              <img src="/rocket-icon.svg" alt="twitter" />
+            </button>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
