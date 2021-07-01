4f1ba687b3277aed312894d8d278a72a17ec6637
Vitor Klein <vitorhklein2@gmail.com>
2021-07-01 18:06:06 -0300

    add metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,23 @@ export default class MyDocument extends Document {
     return(
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
-        <link rel="icon" href="/favicon.ico"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
+          <link rel="icon" href="/favicon.ico"/>
+          <meta name="title" content="Vitor Hugo Klein" />
+          <meta name="description" content="meu portfólio pessoal, espero que gostem :)" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://vitor-klein.vercel.app/" />
+          <meta property="og:title" content="Vitor Hugo Klein" />
+          <meta property="og:description" content="meu portfólio pessoal, espero que gostem :)" />
+          <meta property="og:image" content="" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://vitor-klein.vercel.app/" />
+          <meta property="twitter:title" content="Vitor Hugo Klein" />
+          <meta property="twitter:description" content="meu portfólio pessoal, espero que gostem :)" />
+          <meta property="twitter:image" content="" />
         </Head>
         <body>
           <Main />
