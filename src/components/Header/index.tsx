37a6d627126058337ac2629721efd1211136fe90
Vitor Klein <vitorhklein2@gmail.com>
2021-07-04 14:57:11 -0300

    align header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,8 @@ const Header: React.FC = () => {
     <>
       <div className={styles.header}>
         <header>
-            <h2>VitorK</h2>
+          <h2>VitorK</h2>
+          <div className={styles.devIconsContainer}>
             <div className={styles.devIcons}><br />
               <img alt="Rafa-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" />
               <img alt="Rafa-Ts" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg" />
@@ -18,19 +19,21 @@ const Header: React.FC = () => {
               <img alt="Rafa-CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" />
               <img alt="Rafa-Next" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg" />
             </div>
+          </div>
 
-            <div className={styles.icons}>
-              <Link href="https://www.linkedin.com/in/vitor-klein/">
-                <img src="/linkedin-icon.svg" alt="linkedin"/>
-              </Link>
-              <Link href="https://www.instagram.com/vitor__klein/">
-                <img src="/insta-icon.svg" alt="insta" />
-              </Link>
-              <Link href="https://github.com/Vitor-Klein">
-                <img src="/git-icon.svg" alt="git" />
-              </Link>
-            </div>
-          </header>
+
+          <div className={styles.icons}>
+            <Link href="https://www.linkedin.com/in/vitor-klein/">
+              <img src="/linkedin-icon.svg" alt="linkedin" />
+            </Link>
+            <Link href="https://www.instagram.com/vitor__klein/">
+              <img src="/insta-icon.svg" alt="insta" />
+            </Link>
+            <Link href="https://github.com/Vitor-Klein">
+              <img src="/git-icon.svg" alt="git" />
+            </Link>
+          </div>
+        </header>
       </div>
     </>
   )
