edb2536f8929c4b02ca9f3f96dc624d3af369b20
Vitor Klein <vitorhklein2@gmail.com>
2021-07-04 14:40:26 -0300

    increment header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,6 +9,16 @@ const Header: React.FC = () => {
       <div className={styles.header}>
         <header>
             <h2>VitorK</h2>
+            <div className={styles.devIcons}><br />
+              <img alt="Rafa-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" />
+              <img alt="Rafa-Ts" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg" />
+              <img alt="Rafa-CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" />
+              <img alt="Rafa-React" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" />
+              <img alt="Rafa-HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" />
+              <img alt="Rafa-CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" />
+              <img alt="Rafa-Next" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg" />
+            </div>
+
             <div className={styles.icons}>
               <Link href="https://www.linkedin.com/in/vitor-klein/">
                 <img src="/linkedin-icon.svg" alt="linkedin"/>
