f26dad630b007ebcee42dee99868a345b0775a1a
Vitor Klein <vitorhklein2@gmail.com>
2023-09-09 16:38:29 -0300

    change page and refactor code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import RepoLink from '../components/RepoLink';
 import Links from '../components/Links';
@@ -12,6 +14,20 @@ import repoAnimation from '../assets/git-animation.json';
 import devAnimation from '../assets/development.json';
 
 export default function Home() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/Vitor-Klein/repos')
+      .then(response => {
+        setRepos(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <>
       <div className={styles.container}>
@@ -38,18 +54,21 @@ export default function Home() {
             </div>
           </div>
 
-          <Text
-            title="Eu Profissional"
-            content="Sou um programador dedicado, sempre em busca de aprimorar 
-            minhas habilidades na stack JS/TS. Possuo formação técnica em 
-            informática pelo Colégio Estadual João Manuel Mondrone 
-            de Medianeira, PR e já participei de diversas NLWs, incluindo a 
-            última Semana Omnistack promovida pela Rocketseat. Busco sempre 
-            me manter atualizado com os conteúdos de ponta da Rocketseat e de 
-            outras fontes confiáveis. Fique à vontade para entrar em contato 
-            ou me seguir nas redes sociais.
-            Nunca paro de aprender e estou aberto a novas oportunidades de trabalho."
-          />
+          <div className={styles.myImageContainer}>
+            <h1>Eu pessoal</h1>
+            <div className={styles.myImageContent}>
+              <img src="./imageUnsplash.png" alt="image" />
+              <h2>Sou um programador dedicado, sempre em busca de aprimorar
+                minhas habilidades na stack JS/TS. Possuo formação técnica em
+                informática pelo Colégio Estadual João Manuel Mondrone
+                de Medianeira, PR e já participei de diversas NLWs, incluindo a
+                última Semana Omnistack promovida pela Rocketseat. Busco sempre
+                me manter atualizado com os conteúdos de ponta da Rocketseat e de
+                outras fontes confiáveis. Fique à vontade para entrar em contato
+                ou me seguir nas redes sociais.
+                Nunca paro de aprender e estou aberto a novas oportunidades de trabalho.</h2>
+            </div>
+          </div>
 
           <Animation
             title="Meus Principais Repósitórios"
